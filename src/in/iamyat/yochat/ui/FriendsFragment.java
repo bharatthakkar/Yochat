a55f8300ccd1aa3cce9127b8aa1f035d35050b88
yatintaluja <yatintaluja@gmail.com>
2014-08-07 00:37:13 +0530

    update grid view

--- src/in/iamyat/yochat/ui/FriendsFragment.java
+++ src/in/iamyat/yochat/ui/FriendsFragment.java
@@ -1,20 +1,20 @@
 package in.iamyat.yochat.ui;
 
+import in.iamyat.yochat.adapters.UserAdapter;
 import in.iamyat.yochat.utils.ParseConstants;
 import in.imyat.yochat.R;
-import in.imyat.yochat.R.layout;
-import in.imyat.yochat.R.string;
 
 import java.util.List;
 
 import android.app.AlertDialog;
 import android.os.Bundle;
-import android.support.v4.app.ListFragment;
+import android.support.v4.app.Fragment;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ArrayAdapter;
+import android.widget.GridView;
+import android.widget.TextView;
 
 import com.parse.FindCallback;
 import com.parse.ParseException;
@@ -22,19 +22,27 @@ import com.parse.ParseQuery;
 import com.parse.ParseRelation;
 import com.parse.ParseUser;
 
-public class FriendsFragment extends ListFragment {
+public class FriendsFragment extends Fragment {
 
 	public static final String TAG = FriendsFragment.class.getSimpleName();
 
 	protected List<ParseUser> mFriends;
 	protected ParseRelation<ParseUser> mFriendsRelation;
 	protected ParseUser mCurrentUser;
+	protected GridView mGridView;
 
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
 			Bundle savedInstanceState) {
 		View rootView = inflater.inflate(R.layout.fragment_friends, container,
 				false);
+
+		mGridView = (GridView) rootView.findViewById(R.id.friendsGrid);
+
+		TextView emptyTextView = (TextView) rootView
+				.findViewById(android.R.id.empty);
+		mGridView.setEmptyView(emptyTextView);
+
 		return rootView;
 	}
 
@@ -54,7 +62,7 @@ public class FriendsFragment extends ListFragment {
 
 			@Override
 			public void done(List<ParseUser> friends, ParseException e) {
-				getActivity().setProgressBarIndeterminateVisibility(false);	
+				getActivity().setProgressBarIndeterminateVisibility(false);
 				if (e == null) {
 					mFriends = friends;
 					String[] usernames = new String[mFriends.size()];
@@ -63,14 +71,18 @@ public class FriendsFragment extends ListFragment {
 						usernames[i] = user.getUsername();
 						i++;
 					}
-					ArrayAdapter<String> adapter = new ArrayAdapter<String>(
-							getListView().getContext(),
-							android.R.layout.simple_list_item_1, usernames);
-					setListAdapter(adapter);
+					if (mGridView.getAdapter() == null) {
+						UserAdapter adapter = new UserAdapter(getActivity(),
+								mFriends);
+						mGridView.setAdapter(adapter);
+					} else {
+						((UserAdapter) mGridView.getAdapter()).refill(mFriends);
+					}
+
 				} else {
 					Log.e(TAG, e.getMessage());
 					AlertDialog.Builder builder = new AlertDialog.Builder(
-							getListView().getContext());
+							getActivity());
 					builder.setMessage(e.getMessage())
 							.setTitle(R.string.error_title)
 							.setPositiveButton(android.R.string.ok, null);
